5db4ea8c3c7a89bc29113fc264f29a26df67926f
王知军 <wangzhijun07@meituan.com>
2020-08-04 16:40:21 +0800

    v2

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-   res.send('hello world');
+   res.send('v2');
 })
  
 var server = app.listen(8081, function () {
