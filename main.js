fb390c49022c58f711ae72c922bfb69039fa51a2
王知军 <wangzhijun07@meituan.com>
2020-08-05 21:16:09 +0800

    add more api

--- main.js
+++ main.js
@@ -2,10 +2,32 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-   res.send('v2');
+   res.send('financial v2');
 })
+
+app.get('/loan', function (req, res) {
+  // 使用了superagent来发起请求
+  var superagent = require('superagent');
+  // 查询本机ip，这里需要根据实际情况选择get还是post
+  var sreq = superagent.get('http://loan:8080');
+  sreq.pipe(res);
+  sreq.on('end', function(){
+      console.log('done');
+  });
+});
+
+app.get('/transfer', function (req, res) {
+  // 使用了superagent来发起请求
+  var superagent = require('superagent');
+  // 查询本机ip，这里需要根据实际情况选择get还是post
+  var sreq = superagent.get('http://transfer:8080');
+  sreq.pipe(res);
+  sreq.on('end', function(){
+      console.log('done');
+  });
+});
  
-var server = app.listen(8081, function () {
+var server = app.listen(8080, function () {
  
   var host = server.address().address
   var port = server.address().port
