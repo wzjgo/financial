60cdeec187184b92fa5dead9351aba3dbd3e0d81
王知军 <wangzhijun07@meituan.com>
2020-08-06 11:56:21 +0800

    change port

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-   res.send('v1');
+   res.send('financial v1');
 })
  
 var server = app.listen(8080, function () {
